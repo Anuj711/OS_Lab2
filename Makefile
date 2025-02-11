2f8be431257a93bd352be1ca164a89f2b582a6a4
Anuj711 <anuj.shukla@live.com>
2025-02-11 16:35:20 -0500

    Added group member information to each file that will be submitted

--- Makefile
+++ Makefile
@@ -1,3 +1,13 @@
+#Group Information:
+#CRN: 74025
+#Group 9
+	
+#Group Member 1: Nathaniel Kebere (100868890)
+#Group Member 2: Anuj Shukla (100895219)
+#Group Member 3: Syed Mesam Jafri (100869437)
+#Group Member 4: Farzad Shahdad (100861389)
+#Group Member 5: Danial Shaikh (100698628)
+
 all: myshell
 
 myshell: myshell.c utility.c
--- myshell.c
+++ myshell.c
@@ -1,3 +1,16 @@
+/*
+CRN: 74025
+Group 9
+	
+Group Member 1: Nathaniel Kebere (100868890)
+Group Member 2: Anuj Shukla (100895219)
+Group Member 3: Syed Mesam Jafri (100869437)
+Group Member 4: Farzad Shahdad (100861389)
+Group Member 5: Danial Shaikh (100698628)
+
+*/
+
+
 #include "myshell.h"
 
 
@@ -12,7 +25,8 @@ void execute_command(char *input)
         {
             token = strtok(NULL, delim); // Get next part (directory path)
             change_directory(token);
-        } 
+        }
+        // 
         else 
         {
             printf("Command not recognized.\n");
